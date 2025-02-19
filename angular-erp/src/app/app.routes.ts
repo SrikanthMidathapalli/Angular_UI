f9e54b9100570b0ac0832bab663d27484bbf06c3
Srikanth-Midathapalli-1 <srikanth.midathapalli1@gmail.com>
2025-02-19 16:01:02 -0500

    updated structure by routing and added erro component

--- angular-erp/src/app/app.routes.ts
+++ angular-erp/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent) },
+  { path: 'register', loadComponent: () => import('./auth/register/register.component').then(m => m.RegisterComponent) },
+  { path: 'admin/employees', loadComponent: () => import('./features/admin/employees/employees.component').then(m => m.EmployeesComponent) },
+  { path: 'admin/settings', loadComponent: () => import('./features/admin/settings/settings.component').then(m => m.SettingsComponent)},
+  { path: 'employee/profile', loadComponent: () => import('./features/employee/profile/profile.component').then(m => m.ProfileComponent) },
+  { path: '**', loadComponent: () => import('./shared/not-found/not-found.component').then(m => m.NotFoundComponent) },
+];
+
